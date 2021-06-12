d035c4fc72e960da6f72f3c11544d25f274cc728
Thiago Stteffen <thiagokalu@gmail.com>
2021-06-12 10:41:43 -0300

    completo

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -12,8 +12,13 @@ public class OperadorE {
 		System.out.println("Por favor, digite sua senha:");
 		senha = leitor.next();	
 		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
-		
-		
+		if (usuario.equals("administrador") && senha.equals("123")) {
+			
+			System.out.println("Você é um Administrador com acesso ao sistema"); 
+			
+		} else {
+			System.out.println("Você não possui acesso ao sistema");
+		}	
 		
 		leitor.close();
 
